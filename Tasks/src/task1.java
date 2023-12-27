3aa013cd17440542af07134c5b14393dfe0a5486
Dmitry <124567116+dayerzz@users.noreply.github.com>
2023-12-27 22:13:46 +0300

    commented task1

--- Tasks/src/task1.java
+++ Tasks/src/task1.java
@@ -33,63 +33,68 @@ public class task1 {
         System.out.println(tables(123, 58));
     }
     public static double convert(int gallons) {
-        return gallons * 3.785;
+        return gallons * 3.785; // умножаем кол-во галлонов на американский галлон в литрах
     }
     public static int calc(int time, int intensity) {
-        return time * intensity;
+        return time * intensity; // умножаем время тренировки на интенсивность тренировки
     }
     public static int containers(int box, int bags, int barrel) {
-        return box * 20 + bags * 50 + barrel * 100;
+        return box * 20 + bags * 50 + barrel * 100; // вычисляем общее кол-во товаров на складе 
     }
     public static String triangleType(int x, int y, int z) {
-        if (x + y > z && x + z > y && y + z > x) {
-            if (x == y && y == z) {
-                return "equilateral";
-            } else if (x == y || y == z || x == z) {
-                return "isosceles";
-            } else {
-                return "different_sided";
+        if (x + y > z && x + z > y && y + z > x) { // Проверка условия, что сумма длин двух сторон треугольника больше длины третьей стороны.
+            if (x == y && y == z) { // Проверка, являются ли все три стороны треугольника равными.
+                return "equilateral"; // Если все стороны равны, возвращается строка "equilateral" (равносторонний треугольник).
+            } else if (x == y || y == z || x == z) { // Проверка, является ли хотя бы одна пара сторон треугольника равными.
+                return "isosceles"; // Если хотя бы одна пара сторон равна, возвращается строка "isosceles" (равнобедренный треугольник).
+            } else { // Если треугольник не является ни равносторонним, ни равнобедренным.
+                return "different_sided"; // Возвращается строка "different_sided" (треугольник со сторонами различной длины).
             }
-        } else {
-            return "not a triangle";
+        } else { // Если условие для образования треугольника не выполнено.
+            return "not a triangle"; // Возвращается строка "not a triangle" (не треугольник).
         }
     }
-    public static int ternaryEvalution(int a, int b) {
-        return a > b? a : b;
-    }
-    public static int howManyItems(double length_t, double width, double h) {
-        double s_t = length_t * 2;
-        double s_d = ((width * 2) * (h * 2));
-        return (int) (s_t / s_d);
-    }
-    public static int factorial(int num) {
-        int result = 1;
-        for (int i = 1; i <= num; i++) {
-            result *= i;
-        }
-        return result;
-    }
-    public static int gcd(int x, int y) {
-        while (y != 0) {
-            int result = x % y;
-            x = y;
-            y = result;
-        }
-        return x;
-    }
-    public static int ticketSaler(int ticket, int cost) {
-        return (int) (ticket * (cost - cost * 0.28));
-    }
-    public static int tables(int students, int table) {
-        int need_tables = students / 2;
-        if (students % 2 != 0) {
-            need_tables++;
-        }
-        int result = need_tables - table;
-        if (result < 0) {
-            result = 0;
-        }
-        return result;
 
-    }
+    public static int ternaryEvalution(int a, int b) {
+    return a > b ? a : b; // возвращаем большее из двух чисел a и b при помощи тернарного оператора.
 }
+
+public static int howManyItems(double length_t, double width, double h) {
+    double s_t = length_t * 2; // вычисляем площадь ткани
+    double s_d = ((width * 2) * (h * 2)); // вычисляем площадь детали
+    return (int) (s_t / s_d); // вычисляем кол-во пододеяльников
+}
+
+public static int factorial(int num) {
+    int result = 1; // записываем первый элемент последовательности
+    for (int i = 1; i <= num; i++) { // вычисляем факториал заданного числа
+        result *= i;
+    }
+    return result;
+}
+
+public static int gcd(int x, int y) {
+    while (y != 0) { // пока y не будет равен нулю
+        int result = x % y; // вычисление остатка от деления x на y и сохранение в result
+        x = y;
+        y = result;
+    }
+    return x; / возврат x, которое стало наибольшим общим делителем
+}
+
+public static int ticketSaler(int ticket, int cost) {
+    return (int) (ticket * (cost - cost * 0.28)); // рассчитываем общую выручку от продажи билетов
+}
+
+public static int tables(int students, int table) {
+    int need_tables = students / 2; // делим количество студентов на 2 (т.к. за партой могут сидеть только двое), тем самым находим нужное кол-во парт
+    if (students % 2 != 0) { 
+        need_tables++; // добавляем парту, если кол-во студентов делённое на 2 имеет остаток
+    }
+    int result = need_tables - table; // вычисляем кол-во недостающих парт
+    if (result < 0) {
+        result = 0;
+    }
+    return result;
+}
+// Метод tables вычисляет необходимое количество дополнительных столов для учеников, поделив общее число учеников пополам и округлив в большую сторону. После этого вычисляется разница между необходимым количеством столов и имеющимся, и возвращается это значение (или 0, если не требуется дополнительных столов).
