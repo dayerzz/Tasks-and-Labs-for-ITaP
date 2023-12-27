64ad554ab813863aaf3b22e3271dc9abbdb9d5d1
Dmitry <124567116+dayerzz@users.noreply.github.com>
2023-12-27 20:32:00 +0300

    fix DataProcessor.java

--- Labs/laba8/src/DataProcessor.java
+++ Labs/laba8/src/DataProcessor.java
@@ -4,7 +4,3 @@ import java.lang.annotation.*;
 @Target(ElementType.METHOD)
 public @interface DataProcessor {
 }
-// Эта аннотация предназначена для использования на методах, которые выполняют обработку данных.
-// @Retention(RetentionPolicy.RUNTIME) указывает, что аннотация будет доступна во время выполнения
-// программы, что позволяет использовать ее в рефлексии. @Target(ElementType.METHOD) ограничивает
-// использование аннотации только методами.
