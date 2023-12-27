a457f5d2195cad2808a4bd2ff42896ff6502b8b4
Dmitry <124567116+dayerzz@users.noreply.github.com>
2023-12-27 23:34:49 +0300

    commented task4

--- Tasks/src/task4.java
+++ Tasks/src/task4.java
@@ -28,32 +28,50 @@ public class task4 {
         System.out.println(switchNums(6274, 71259));
     }
     public static String nonRepeatable(String str) {
-        StringBuilder result = new StringBuilder();
-        Set<Object> charSet = new HashSet<>();
+        StringBuilder result = new StringBuilder(); // Создаем StringBuilder для хранения уникальных символов
+        Set<Object> charSet = new HashSet<>(); // Создаем множество для отслеживания уникальных символов
+
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
+            // Перебираем каждый символ в строке
+
             if (!charSet.contains(c)) {
-                result.append(c);
-                charSet.add(c);
+                // Проверяем, есть ли символ в множестве
+                result.append(c); // Если символ уникален, добавляем его в результат
+                charSet.add(c); // Добавляем символ в множество
             }
         }
-        return result.toString();
+
+        return result.toString(); // Возвращаем строку, содержащую только уникальные символы
     }
     public static List<String> generateBrackets(int n) {
-        List<String> result= new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        // Создаем список для хранения результатов
         backtrack(result, "", 0, 0, n);
+        // Запускаем рекурсивный метод backtrack для генерации скобочных последовательностей
+
         return result;
+        // Возвращаем список сгенерированных скобочных последовательностей
     }
+
     private static void backtrack(List<String> result, String current, int open, int close, int max) {
         if (current.length() == 2 * max) {
+            // Если текущая последовательность достигла нужной длины (2 * max),
+            // добавляем ее в список результатов и завершаем рекурсию
             result.add(current);
             return;
         }
+
         if (open < max) {
+            // Если количество открывающих скобок меньше максимального разрешенного,
+            // добавляем открывающую скобку и вызываем backtrack для продолжения поиска
             backtrack(result, current + "(", open + 1, close, max);
         }
+
         if (close < open) {
-            backtrack(result,current + ")", open, close + 1, max);
+            // Если количество закрывающих скобок меньше количества открывающих,
+            // добавляем закрывающую скобку и вызываем backtrack для продолжения поиска
+            backtrack(result, current + ")", open, close + 1, max);
         }
     }
     // 3
@@ -126,7 +144,7 @@ public class task4 {
         return result.toString();
     }
     public static int convertToNum(String numberString) {
-        Map<String, Integer> numberMap = new HashMap<>();
+        Map<String, Integer> numberMap = new HashMap<>(); // Создаем Map для связи словенного представления чисел с их числовыми значениями
         numberMap.put("zero", 0);
         numberMap.put("one", 1);
         numberMap.put("two", 2);
@@ -156,33 +174,39 @@ public class task4 {
         numberMap.put("eighty", 80);
         numberMap.put("ninety", 90);
         numberMap.put("hundred", 100);
-
-        String[] words = numberString.split(" ");
-        int current = 0;
+        // Заполняем Map значениями от "zero" до "hundred" соответствующими числовыми значениями
+        String[] words = numberString.split(" "); // Разбиваем строку на слова
+        int current = 0; // Инициализируем переменную для хранения текущего числа
 
         for (String word : words) {
-            int value = numberMap.get(word);
+            int value = numberMap.get(word); // Получаем числовое значение слова из Map
             if (value == 100) {
-                current = current * value;
+                current = current * value; // Если слово - "hundred", умножаем текущее число на 100
             } else {
-                current += value;
+                current += value; // В противном случае, добавляем числовое значение слова к текущему числу
             }
         }
-        return current;
+        return current; // Возвращаем число, представленное в словесной форме в числовом виде
     }
     public static String uniqueSubstring(String num) {
         String nums = "0123456789";
         String result = "";
         for (int x = 0; x <= num.length(); x++) {
             for (int y = x; y <= num.length(); y++) {
+                // Перебираем все возможные подстроки из исходной строки
                 if (nums.contains(num.substring(x, y))) {
+                    // Проверяем, содержит ли подстрока только уникальные цифры
                     if (result.length() < num.substring(x, y).length()) {
+                        // Если подстрока содержит уникальные цифры и длина больше текущего результата
+                        // Обновляем результат на текущую подстроку
                         result = num.substring(x, y);
                     }
                 }
             }
         }
+
         return result;
+        // Возвращаем самую длинную подстроку с уникальными цифрами
     }
     public static int shortestWay(int[][] matrix) {
         int n = matrix.length; // Длина матрицы
@@ -202,16 +226,20 @@ public class task4 {
         return result[n - 1][n - 1];
     }
     public static String numericOrder(String str) {
-        StringBuilder result = new StringBuilder();
-        String[] string = str.split(" ");
+        StringBuilder result = new StringBuilder(); // Создаем StringBuilder для формирования результата
+        String[] string = str.split(" "); // Разбиваем строку на слова по пробелам
         for (int i = 1; i <= string.length; i++) {
+            // Проходим по числам от 1 до длины массива слов string
             for (String word : string) {
+                // Перебираем каждое слово в массиве
                 if (word.contains(String.valueOf(i))) {
+                    // Если текущее слово содержит текущее число
                     result.append(word.replaceAll("\\d", "")).append(" ");
+                    // Добавляем это слово в результат без числа и пробел в конце
                 }
             }
         }
-        return result.toString();
+        return result.toString(); // Возвращаем полученную строку
     }
     public static int switchNums(int n1, int n2) {
         char[] n1Char = String.valueOf(n1).toCharArray();
