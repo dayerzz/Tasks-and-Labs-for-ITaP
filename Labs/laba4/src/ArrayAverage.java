0e6b912b1842301aa6340ff4e7f8550caf55543e
Dmitry <124567116+dayerzz@users.noreply.github.com>
2023-12-27 20:27:53 +0300

    fix ArrayAverage.java

--- Labs/laba4/src/ArrayAverage.java
+++ Labs/laba4/src/ArrayAverage.java
@@ -1,20 +1,3 @@
-//public class ArrayAverage {
-//    public static void main(String[] args) {
-//        int[] arr = {1, 2, 3, 4, 5};
-//        int sum = 0;
-//        try {
-//            for (int i = 0; i <= arr.length; i++) {
-//                sum += i;
-//            }
-//            double result = (double) sum / arr.length;
-//            System.out.println(result);
-//        }   catch (ArrayIndexOutOfBoundsException e) {
-//            System.out.println("Ошибка: произошёл выход за пределы массива");
-//        }   catch (NumberFormatException e) {
-//            System.out.println("Ошибка: элемент массива не является числом");
-//        }
-//    }
-//}
 public class ArrayAverage {
     public static void main(String[] args) {
         String[] arr = {"1", "2", "three", "4", "5"}; // Изменен тип массива на String и добавлено "three"
