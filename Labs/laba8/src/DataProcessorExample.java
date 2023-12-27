7c0332543a4bbe7f00508e6c909d8b8c8bb55179
Dmitry <124567116+dayerzz@users.noreply.github.com>
2023-12-27 20:32:14 +0300

    fix DataProcessorExample.java

--- Labs/laba8/src/DataProcessorExample.java
+++ Labs/laba8/src/DataProcessorExample.java
@@ -6,5 +6,3 @@ public class DataProcessorExample {
         return "Результат: " + data + " Java!";
     }
 }
-//Этот класс содержит метод обработки данных, помеченный аннотацией @DataProcessor.
-// Метод processData просто добавляет префикс "Результат: " к каждой строке данных.
